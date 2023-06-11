b2e2ffe15fcdcd949ae5e350c27cd4877c5b12c5
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-06-11 23:30:00 +0530

    removed BufWinEnter autocmd

--- lua/hologram-math-preview/autocmds.lua
+++ lua/hologram-math-preview/autocmds.lua
@@ -2,7 +2,8 @@ local hologram_math_preview = require("hologram-math-preview")
 
 local augroup = vim.api.nvim_create_augroup("Hologram-math-preview", { clear = true })
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+-- "BufEnter"
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
 	pattern = { "*.md" },
 	group = augroup,
 	callback = function(ev)
